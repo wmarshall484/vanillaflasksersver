4754e9196023d8b9d3ebc4b266d250f32a2ed9f2
Will Marshall <wmarshall484@gmail.com>
2018-07-23 10:22:45 -0700

    asdf

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/mpg', method = ['GET'])
+@app.route('/mpg', methods = ['GET'])
 def mpg():
     return render_template('mpg.html')
 
